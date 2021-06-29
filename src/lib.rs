4762be89b2d5c33748053bf720f3fe92ad3eb70e
ray33ee <30669752+ray33ee@users.noreply.github.com>
2021-06-29 23:27:43 +0100

    Version 0.0.2 - Result added to serialize and deserialize functions
    Signed-off-by: ray33ee <30669752+ray33ee@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -11,30 +11,60 @@ use std::borrow::{BorrowMut};
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use chrono::{DateTime, Local, TimeZone};
 
-//BINCODE_OPTIONS represents options for the bincode functions that serialise data into a format compatible with the Tor specification
+type Result<T> = std::result::Result<T, ErrorKind>;
+
+#[derive(Debug)]
+pub enum ErrorKind {
+    /// An unrecognised command with the value of the command and the bytes left still to read
+    InvalidCommand(u8, u64),
+
+    /// A call to a bincode function failed
+    BincodeError(bincode::ErrorKind),
+
+    /// A Read/Write/Seek call failed (write_all is used in the for Strings)
+    StdIoError(std::io::ErrorKind),
+
+    /// A predicted digest does not match the actual. Predicted and actual digests plus the number of bytes left to read
+    BadDigest(u32, u32),
+}
+
+impl From<Box<bincode::ErrorKind>> for ErrorKind {
+    fn from(error: Box<bincode::ErrorKind>) -> Self {
+        ErrorKind::BincodeError(*error)
+    }
+}
+
+impl From<std::io::Error> for ErrorKind {
+    fn from(error: std::io::Error) -> Self {
+        ErrorKind::StdIoError(error.kind())
+    }
+}
+
+/// Represents options for the bincode functions that serialise data into a format compatible with the Tor specification
 lazy_static! {
     static ref BINCODE_OPTIONS: WithOtherEndian<WithOtherIntEncoding<DefaultOptions, FixintEncoding>, BigEndian> = bincode::config::DefaultOptions::new().with_fixint_encoding().with_big_endian();
 }
 
-//TorSerde trait exposes functions that serialise and deserialise data in accordance with the Tor specification
+///TorSerde trait exposes functions that serialise and deserialise data in accordance with the Tor specification
 pub trait TorSerde {
 
-    fn bin_serialise_into<W: Write>(&self, stream: W);
+    ///Return the length of the data when serialised, in bytes. Used when the length of the payload is required
+    fn bin_serialise_into<W: Write>(&self, stream: W) -> Result<u32>;
 
-    //The payload_length argument is literally only needed for the Versions cell since its length is not a part  of the payload
-    fn bin_deserialise_from<R: Read>(stream: R) -> Self;
+    fn bin_deserialise_from<R: Read>(stream: R) -> Result<Self> where Self: Sized;
 
-    //Return the length of the data when serialised, in bytes. Used when the length of the payload is required
     fn serialised_length(&self) -> u32;
 }
 
-//A wrapper used to handle the Versions cell payload, which (unlike pretty much all other variable length cell payloads) does not contain its own length
+///A wrapper used to handle the Versions cell payload, which (unlike pretty much all other variable length cell payloads) does not contain its own length
 #[derive(Debug, Clone)]
-pub struct VersionsVector(Vec<u16>);
+pub struct VersionsVector(pub Vec<u16>);
 
-//A thin wrapper around Vec that allows us to specify the number of bytes to serialise the length of the vector into (N)
+///A thin wrapper around Vec that allows us to specify the number of bytes to serialise the length of the vector into (N)
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct NLengthVector<T: TorSerde, const N: usize>(Vec<T>);
+pub struct NLengthVector<T: TorSerde, const N: usize>(pub Vec<T>);
+
+///A thin wrapper around the 4-byte digest object used in relay cells?
 
 impl From<Vec<u16>> for VersionsVector {
     fn from(v: Vec<u16>) -> Self {
@@ -61,113 +91,130 @@ impl<T: TorSerde, const N: usize> From<NLengthVector<T, N>> for Vec<T> {
 }
 
 impl TorSerde for u8 {
-    fn bin_serialise_into<W: Write>(&self, stream: W) {
-        BINCODE_OPTIONS.serialize_into(stream, &self).unwrap();
+    fn bin_serialise_into<W: Write>(&self, stream: W) -> Result<u32> {
+        BINCODE_OPTIONS.serialize_into(stream, &self)?;
+        Ok(self.serialised_length())
     }
 
-    fn bin_deserialise_from<R: Read>(stream: R) -> Self {
-        BINCODE_OPTIONS.deserialize_from(stream).unwrap()
+    fn bin_deserialise_from<R: Read>(stream: R) -> Result<Self> {
+        let res: Self = BINCODE_OPTIONS.deserialize_from(stream)?;
+        Ok(res)
     }
 
-    fn serialised_length(&self) -> u32 { 1 }
+    fn serialised_length(&self) -> u32 {
+        1
+    }
+
 }
 
 impl TorSerde for u16 {
-    fn bin_serialise_into<W: Write>(&self, stream: W) {
-        BINCODE_OPTIONS.serialize_into(stream, &self).unwrap();
+    fn bin_serialise_into<W: Write>(&self, stream: W) -> Result<u32> {
+        BINCODE_OPTIONS.serialize_into(stream, &self)?;
+        Ok(self.serialised_length())
     }
 
-    fn bin_deserialise_from<R: Read>(stream: R) -> Self {
-        BINCODE_OPTIONS.deserialize_from(stream).unwrap()
+    fn bin_deserialise_from<R: Read>(stream: R) -> Result<Self> {
+        let res: Self = BINCODE_OPTIONS.deserialize_from(stream)?;
+        Ok(res)
     }
 
-    fn serialised_length(&self) -> u32 { 2 }
+    fn serialised_length(&self) -> u32 {
+        2
+    }
 }
 
 impl TorSerde for u32 {
-    fn bin_serialise_into<W: Write>(&self, stream: W) {
-        BINCODE_OPTIONS.serialize_into(stream, &self).unwrap();
+    fn bin_serialise_into<W: Write>(&self, stream: W) -> Result<u32> {
+        BINCODE_OPTIONS.serialize_into(stream, &self)?;
+        Ok(self.serialised_length())
     }
 
-    fn bin_deserialise_from<R: Read>(stream: R) -> Self {
-        BINCODE_OPTIONS.deserialize_from(stream).unwrap()
+    fn bin_deserialise_from<R: Read>(stream: R) -> Result<Self> {
+        let res: Self = BINCODE_OPTIONS.deserialize_from(stream)?;
+        Ok(res)
     }
 
-    fn serialised_length(&self) -> u32 { 4 }
+    fn serialised_length(&self) -> u32 {
+        4
+    }
 }
 
 impl TorSerde for u128 {
-    fn bin_serialise_into<W: Write>(&self, stream: W) {
-        BINCODE_OPTIONS.serialize_into(stream, &self).unwrap();
+    fn bin_serialise_into<W: Write>(&self, stream: W) -> Result<u32> {
+        BINCODE_OPTIONS.serialize_into(stream, &self)?;
+        Ok(self.serialised_length())
     }
 
-    fn bin_deserialise_from<R: Read>(stream: R) -> Self {
-        BINCODE_OPTIONS.deserialize_from(stream).unwrap()
+    fn bin_deserialise_from<R: Read>(stream: R) -> Result<Self> {
+        let res: Self = BINCODE_OPTIONS.deserialize_from(stream)?;
+        Ok(res)
     }
 
-    fn serialised_length(&self) -> u32 { 16 }
+    fn serialised_length(&self) -> u32 {
+        16
+    }
 }
 
 impl<T: TorSerde, const N: usize> TorSerde for NLengthVector<T, N> {
-    fn bin_serialise_into<W: Write>(&self, mut stream: W) {
-        match N {
-            1 => (self.0.len() as u8).bin_serialise_into(stream.borrow_mut()),
-            2 => (self.0.len() as u16).bin_serialise_into(stream.borrow_mut()),
-            4 => (self.0.len() as u8).bin_serialise_into(stream.borrow_mut()),
+    fn bin_serialise_into<W: Write>(&self, mut stream: W) -> Result<u32> {
+        //let mut total = N as u32;
+
+        let mut total = match N {
+            1 => (self.0.len() as u8).bin_serialise_into(stream.borrow_mut())?,
+            2 => (self.0.len() as u16).bin_serialise_into(stream.borrow_mut())?,
+            4 => (self.0.len() as u32).bin_serialise_into(stream.borrow_mut())?,
             _ => unreachable!()
-        }
+        };
 
         for item in self.0.iter() {
-            item.bin_serialise_into(stream.borrow_mut());
+            total += item.bin_serialise_into(stream.borrow_mut())?;
         }
+
+        Ok(total)
     }
 
-    fn bin_deserialise_from<R: Read>(mut stream: R) -> Self {
+    fn bin_deserialise_from<R: Read>(mut stream: R) -> Result<Self> {
         let length = match N {
-            1 => u8::bin_deserialise_from(stream.borrow_mut()) as u32,
-            2 => u16::bin_deserialise_from(stream.borrow_mut()) as u32,
-            4 => u32::bin_deserialise_from(stream.borrow_mut()),
+            1 => u8::bin_deserialise_from(stream.borrow_mut())? as u32,
+            2 => u16::bin_deserialise_from(stream.borrow_mut())? as u32,
+            4 => u32::bin_deserialise_from(stream.borrow_mut())?,
             _ => unreachable!()
         };
 
         let mut list = Vec::with_capacity(length as usize);
 
         for _ in 0..length {
-            list.push(T::bin_deserialise_from(stream.borrow_mut()));
+            list.push(T::bin_deserialise_from(stream.borrow_mut())?);
         }
 
-        Self(list)
+        Ok(Self(list))
     }
 
     fn serialised_length(&self) -> u32 {
-        let mut total = N as u32;
-
-        for item in self.0.iter() {
-            total += item.serialised_length();
-        }
-
-        total
+        self.0.iter().fold(N as u32, |acc, x| acc + x.serialised_length())
     }
 }
 
 impl TorSerde for VersionsVector {
-    fn bin_serialise_into<W: Write>(&self, mut stream: W) {
-        ((self.0.len()*2) as u16).bin_serialise_into(stream.borrow_mut());
+    fn bin_serialise_into<W: Write>(&self, mut stream: W) -> Result<u32> {
+        ((self.0.len()*2) as u16).bin_serialise_into(stream.borrow_mut())?;
 
         for item in self.0.iter() {
-            item.bin_serialise_into(stream.borrow_mut())
+            item.bin_serialise_into(stream.borrow_mut())?;
         }
+
+        Ok(self.serialised_length())
     }
 
-    fn bin_deserialise_from<R: Read>(mut stream: R) -> Self {
-        let mut length = u16::bin_deserialise_from(stream.borrow_mut()) / 2;
+    fn bin_deserialise_from<R: Read>(mut stream: R) -> Result<Self> {
+        let length = u16::bin_deserialise_from(stream.borrow_mut())? / 2;
         let mut list = Vec::with_capacity(length as usize);
 
         for _ in 0..length {
-            list.push(u16::bin_deserialise_from(stream.borrow_mut()));
+            list.push(u16::bin_deserialise_from(stream.borrow_mut())?);
         }
 
-        Self(list)
+        Ok(Self(list))
     }
 
     fn serialised_length(&self) -> u32 {
@@ -176,14 +223,14 @@ impl TorSerde for VersionsVector {
 }
 
 impl TorSerde for DateTime<Local> {
-    fn bin_serialise_into<W: Write>(&self, stream: W) {
+    fn bin_serialise_into<W: Write>(&self, stream: W) -> Result<u32> {
         (self.timestamp() as u32).bin_serialise_into(stream)
     }
 
-    fn bin_deserialise_from<R: Read>(stream: R) -> Self {
-        let timestamp = u32::bin_deserialise_from(stream);
+    fn bin_deserialise_from<R: Read>(stream: R) -> Result<Self> {
+        let timestamp = u32::bin_deserialise_from(stream)?;
 
-        Local.timestamp(timestamp as i64, 0)
+        Ok(Local.timestamp(timestamp as i64, 0))
     }
 
     fn serialised_length(&self) -> u32 {
@@ -192,16 +239,16 @@ impl TorSerde for DateTime<Local> {
 }
 
 impl TorSerde for Ipv4Addr {
-    fn bin_serialise_into<W: Write>(&self, stream: W) {
+    fn bin_serialise_into<W: Write>(&self, stream: W) -> Result<u32> {
         let bytes = u32::from(self.clone());
 
         bytes.bin_serialise_into(stream)
     }
 
-    fn bin_deserialise_from<R: Read>(stream: R) -> Self {
-        let bytes = u32::bin_deserialise_from(stream);
+    fn bin_deserialise_from<R: Read>(stream: R) -> Result<Self> {
+        let bytes = u32::bin_deserialise_from(stream)?;
 
-        Self::from(bytes)
+        Ok(Self::from(bytes))
     }
 
     fn serialised_length(&self) -> u32 {
@@ -210,16 +257,16 @@ impl TorSerde for Ipv4Addr {
 }
 
 impl TorSerde for Ipv6Addr {
-    fn bin_serialise_into<W: Write>(&self, stream: W) {
+    fn bin_serialise_into<W: Write>(&self, stream: W) -> Result<u32> {
         let bytes = u128::from(self.clone());
 
         bytes.bin_serialise_into(stream)
     }
 
-    fn bin_deserialise_from<R: Read>(stream: R) -> Self {
-        let bytes = u128::bin_deserialise_from(stream);
+    fn bin_deserialise_from<R: Read>(stream: R) -> Result<Self> {
+        let bytes = u128::bin_deserialise_from(stream)?;
 
-        Self::from(bytes)
+        Ok(Self::from(bytes))
     }
 
     fn serialised_length(&self) -> u32 {
@@ -228,35 +275,35 @@ impl TorSerde for Ipv6Addr {
 }
 
 impl TorSerde for IpAddr {
-    fn bin_serialise_into<W: Write>(&self, mut stream: W) {
-        match self {
+    fn bin_serialise_into<W: Write>(&self, mut stream: W) -> Result<u32> {
+        Ok(match self {
             IpAddr::V4(ipv4) => {
-                4u8.bin_serialise_into(stream.borrow_mut()); //Address type (4 for ipv4, 6 for ipv6)
-                4u8.bin_serialise_into(stream.borrow_mut()); //Address length (4 for ipv4, 16 for ipv6)
-                ipv4.bin_serialise_into(stream.borrow_mut());
+                4u8.bin_serialise_into(stream.borrow_mut())? + //Address type (4 for ipv4, 6 for ipv6)
+                4u8.bin_serialise_into(stream.borrow_mut())? + //Address length (4 for ipv4, 16 for ipv6)
+                ipv4.bin_serialise_into(stream.borrow_mut())?
             }
             IpAddr::V6(ipv6) => {
-                6u8.bin_serialise_into(stream.borrow_mut()); //Address type (4 for ipv4, 6 for ipv6)
-                16u8.bin_serialise_into(stream.borrow_mut()); //Address length (4 for ipv4, 16 for ipv6)
-                ipv6.bin_serialise_into(stream.borrow_mut());
+                6u8.bin_serialise_into(stream.borrow_mut())? + //Address type (4 for ipv4, 6 for ipv6)
+                16u8.bin_serialise_into(stream.borrow_mut())? + //Address length (4 for ipv4, 16 for ipv6)
+                ipv6.bin_serialise_into(stream.borrow_mut())?
             }
-        }
+        })
     }
 
-    fn bin_deserialise_from<R: Read>(mut stream: R) -> Self {
-        let atype = u8::bin_deserialise_from(stream.borrow_mut());
+    fn bin_deserialise_from<R: Read>(mut stream: R) -> Result<Self> {
+        let atype = u8::bin_deserialise_from(stream.borrow_mut())?;
 
-        let _alen = u8::bin_deserialise_from(stream.borrow_mut());
+        let _alen = u8::bin_deserialise_from(stream.borrow_mut())?;
 
-        match atype {
-            4 => { Self::V4(Ipv4Addr::bin_deserialise_from(stream.borrow_mut())) }
-            6 => { Self::V6(Ipv6Addr::bin_deserialise_from(stream.borrow_mut())) }
+        Ok(match atype {
+            4 => { Self::V4(Ipv4Addr::bin_deserialise_from(stream.borrow_mut())?) }
+            6 => { Self::V6(Ipv6Addr::bin_deserialise_from(stream.borrow_mut())?) }
             _ => unreachable!()
-        }
+        })
     }
 
     fn serialised_length(&self) -> u32 {
-        2 + match self {
+        2 + match &self {
             IpAddr::V4(ipv4) => {ipv4.serialised_length()}
             IpAddr::V6(ipv6) => {ipv6.serialised_length()}
         }
@@ -264,19 +311,21 @@ impl TorSerde for IpAddr {
 }
 
 impl TorSerde for String {
-    fn bin_serialise_into<W: Write>(&self, mut stream: W) {
+    fn bin_serialise_into<W: Write>(&self, mut stream: W) -> Result<u32> {
         //Write the contents of the string to the stream
-        stream.borrow_mut().write_all(self.as_bytes()).unwrap();
+        stream.borrow_mut().write_all(self.as_bytes())?;
 
         //Append the stream with the null terminator
-        0u8.bin_serialise_into(stream.borrow_mut())
+        0u8.bin_serialise_into(stream.borrow_mut());
+
+        Ok(self.serialised_length())
     }
 
-    fn bin_deserialise_from<R: Read>(mut stream: R) -> Self {
+    fn bin_deserialise_from<R: Read>(mut stream: R) -> Result<Self> {
         let mut string = Vec::new();
 
         loop {
-            let byte = u8::bin_deserialise_from(stream.borrow_mut());
+            let byte = u8::bin_deserialise_from(stream.borrow_mut())?;
 
             if byte == 0 {
                 break;
@@ -286,33 +335,35 @@ impl TorSerde for String {
         }
 
         unsafe {
-            Self::from_utf8_unchecked(string)
+            Ok(Self::from_utf8_unchecked(string))
         }
     }
 
     fn serialised_length(&self) -> u32 {
-        self.len() as u32 + 1
+        1 + self.len() as u32
     }
 }
 
 impl<const N: usize> TorSerde for [u8; N] {
-    fn bin_serialise_into<W: Write>(&self, mut stream: W) {
+    fn bin_serialise_into<W: Write>(&self, mut stream: W) -> Result<u32> {
         for item in self.iter() {
-            item.bin_serialise_into(stream.borrow_mut())
+            item.bin_serialise_into(stream.borrow_mut())?;
         }
+
+        Ok(N as u32)
     }
 
-    fn bin_deserialise_from<R: Read>(mut stream: R) -> Self {
+    fn bin_deserialise_from<R: Read>(mut stream: R) -> Result<Self> {
         let mut array = [0u8; N];
 
         for item in array.iter_mut() {
-            *item = u8::bin_deserialise_from(stream.borrow_mut());
+            *item = u8::bin_deserialise_from(stream.borrow_mut())?;
         }
 
-        array
+        Ok(array)
     }
 
     fn serialised_length(&self) -> u32 {
-        N as u32
+        self.len() as u32
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -12,11 +12,11 @@ mod tests {
 
         let number = 0x45u8;
 
-        number.bin_serialise_into(& mut buffer);
+        number.bin_serialise_into(& mut buffer).unwrap();
 
         assert_eq!(buffer, [0x45]);
 
-        let d_result = u8::bin_deserialise_from(buffer.as_slice());
+        let d_result = u8::bin_deserialise_from(buffer.as_slice()).unwrap();
 
         assert_eq!(d_result, number);
     }
@@ -27,11 +27,11 @@ mod tests {
 
         let number = 0x39e3u16;
 
-        number.bin_serialise_into(& mut buffer);
+        number.bin_serialise_into(& mut buffer).unwrap();
 
         assert_eq!(buffer, [0x39, 0xe3]);
 
-        let d_result = u16::bin_deserialise_from(buffer.as_slice());
+        let d_result = u16::bin_deserialise_from(buffer.as_slice()).unwrap();
 
         assert_eq!(d_result, number);
     }
@@ -42,11 +42,11 @@ mod tests {
 
         let number = 0x7e38d1a0u32;
 
-        number.bin_serialise_into(&mut buffer);
+        number.bin_serialise_into(&mut buffer).unwrap();
 
         assert_eq!(buffer, [0x7e, 0x38, 0xd1, 0xa0]);
 
-        let d_result = u32::bin_deserialise_from(buffer.as_slice());
+        let d_result = u32::bin_deserialise_from(buffer.as_slice()).unwrap();
 
         assert_eq!(d_result, number);
     }
@@ -58,11 +58,11 @@ mod tests {
 
         let number = 0x298bf077459127438fe12329707bcd4bu128;
 
-        number.bin_serialise_into(& mut buffer);
+        number.bin_serialise_into(& mut buffer).unwrap();
 
         assert_eq!(buffer, [0x29, 0x8b, 0xf0, 0x77, 0x45, 0x91, 0x27, 0x43, 0x8f, 0xe1, 0x23, 0x29, 0x70, 0x7b, 0xcd, 0x4b]);
 
-        let d_result = u128::bin_deserialise_from(buffer.as_slice());
+        let d_result = u128::bin_deserialise_from(buffer.as_slice()).unwrap();
 
         assert_eq!(d_result, number);
     }
@@ -74,11 +74,11 @@ mod tests {
 
         let time = Local.timestamp(1431648000, 0);
 
-        time.bin_serialise_into(& mut buffer);
+        time.bin_serialise_into(& mut buffer).unwrap();
 
         assert_eq!(buffer, [85, 85, 55, 0]);
 
-        let d_time = DateTime::<Local>::bin_deserialise_from(buffer.as_slice());
+        let d_time = DateTime::<Local>::bin_deserialise_from(buffer.as_slice()).unwrap();
 
         assert_eq!(d_time, time);
     }
@@ -91,11 +91,11 @@ mod tests {
 
         let mut buffer = Vec::new();
 
-        wrap.bin_serialise_into(& mut buffer);
+        wrap.bin_serialise_into(& mut buffer).unwrap();
 
         assert_eq!(buffer, [6u8, 0, 0, 0, 23, 0, 86, 0, 35, 0, 96, 0, 83]);
 
-        let d_wrap = NLengthVector::<u16, 1>::bin_deserialise_from(buffer.as_slice());
+        let d_wrap = NLengthVector::<u16, 1>::bin_deserialise_from(buffer.as_slice()).unwrap();
 
         assert_eq!(Vec::from(wrap), Vec::from(d_wrap))
 
@@ -107,11 +107,11 @@ mod tests {
 
         let array = [0u8, 54, 34, 85, 78, 45, 8];
 
-        array.bin_serialise_into(& mut buffer);
+        array.bin_serialise_into(& mut buffer).unwrap();
 
         assert_eq!(buffer, array);
 
-        let d_array = <[u8; 7]>::bin_deserialise_from(buffer.as_slice());
+        let d_array = <[u8; 7]>::bin_deserialise_from(buffer.as_slice()).unwrap();
 
         assert_eq!(d_array, array)
 
@@ -123,11 +123,11 @@ mod tests {
 
         let ipv4 = Ipv4Addr::new(245, 67, 12, 34);
 
-        ipv4.bin_serialise_into(& mut buffer);
+        ipv4.bin_serialise_into(& mut buffer).unwrap();
 
         assert_eq!(buffer, [245, 67, 12, 34]);
 
-        let d_ipv4 = Ipv4Addr::bin_deserialise_from(buffer.as_slice());
+        let d_ipv4 = Ipv4Addr::bin_deserialise_from(buffer.as_slice()).unwrap();
 
         assert_eq!(d_ipv4, ipv4);
     }
@@ -138,11 +138,11 @@ mod tests {
 
         let ipv6 = Ipv6Addr::from_str("fc86:6e01:204f:498a:33cf:b30a:6171:e74f").unwrap();
 
-        ipv6.bin_serialise_into(& mut buffer);
+        ipv6.bin_serialise_into(& mut buffer).unwrap();
 
         assert_eq!(buffer, [0xfc, 0x86, 0x6e, 0x01, 0x20, 0x4f, 0x49, 0x8a, 0x33, 0xcf, 0xb3, 0x0a, 0x61, 0x71, 0xe7, 0x4f]);
 
-        let d_ipv6 = Ipv6Addr::bin_deserialise_from(buffer.as_slice());
+        let d_ipv6 = Ipv6Addr::bin_deserialise_from(buffer.as_slice()).unwrap();
 
         assert_eq!(d_ipv6, ipv6);
     }
@@ -153,11 +153,11 @@ mod tests {
 
         let ip = IpAddr::V4(Ipv4Addr::from_str("227.82.127.3").unwrap());
 
-        ip.bin_serialise_into(& mut buffer);
+        ip.bin_serialise_into(& mut buffer).unwrap();
 
         assert_eq!(buffer, [4, 4, 227, 82, 127, 3]);
 
-        let d_ip = IpAddr::bin_deserialise_from(buffer.as_slice());
+        let d_ip = IpAddr::bin_deserialise_from(buffer.as_slice()).unwrap();
 
         assert_eq!(ip, d_ip);
     }
@@ -168,11 +168,11 @@ mod tests {
 
         let string = String::from("abcdefg");
 
-        string.bin_serialise_into(& mut buffer);
+        string.bin_serialise_into(& mut buffer).unwrap();
 
         assert_eq!(buffer, [97, 98, 99, 100, 101, 102, 103, 0]);
 
-        let d_string = String::bin_deserialise_from(buffer.as_slice());
+        let d_string = String::bin_deserialise_from(buffer.as_slice()).unwrap();
 
         assert_eq!(string, d_string);
     }
